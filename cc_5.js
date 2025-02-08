93397567b74340f5e386465f6025fbbfc6e777dd
Eugene Rodriguez <er11@usf.edu>
2025-02-08 13:14:29 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,14 @@ const customer = {
 console.log(`Name: ${customer.name}`); // logging name of customer
 console.log(`Age: ${customer.age}`); // logging age of customer
 console.log(`Email: ${customer.email}`); // logging email of customer
+
+// Task 2: Object Methods
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {
+        console.log(`Order Id: ${this.orderId}, Total: $${this.totalAmount}, Status: ${this.status}`);
+    }
+}; // Console log of customer order with details
+order.displayOrder();
