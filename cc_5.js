5e29037d1a9cb8a47287535e327de89484853bd3
Eugene Rodriguez <er11@usf.edu>
2025-02-08 13:02:45 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+}; // object as customer
+
+console.log(`Name: ${customer.name}`); // logging name of customer
+console.log(`Age: ${customer.age}`); // logging age of customer
+console.log(`Email: ${customer.email}`); // logging email of customer
